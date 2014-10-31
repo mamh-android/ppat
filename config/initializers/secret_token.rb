b230c01a3163b01b37e4e41bb35698a2909a5f53
Lizhen Zhou <zhoulz@marvell.com>
2014-10-31 17:02:59 +0800

    init
    Change-Id: I6e2497b5dbc9438dc01f40461e29bc5cf822b3cb
    Signed-off-by: Lizhen Zhou <zhoulz@marvell.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppat::Application.config.secret_token = '40b48089b8c7e5d88cb95ae76cb76ce2c4b1153d19645846f4f58920a65f7089b68b58053191aecbb2f3da95f4aecc99ee7c1489a9888f7ce2dccf83f577794f'
